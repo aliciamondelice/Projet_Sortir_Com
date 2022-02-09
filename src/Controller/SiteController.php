38e08895754c7dfb5509d3107a3ea948c5204d08
WillyWnkr <thomas.ferrer2021@campus-eni.fr>
2022-02-09 14:44:19 +0100

    Ajout nouveau site

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Site;
+use App\Form\SiteType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,4 +19,26 @@ class SiteController extends AbstractController
             'controller_name' => 'SiteController',
         ]);
     }
+
+    #[Route('/new_site', name:'new_site')]
+
+    public function newSite(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $site = new Site();
+        $formSite = $this->createForm(SiteType::class, $site);
+        $formSite->handleRequest($request);
+
+        if ($formSite->isSubmitted() && $formSite->isValid()) {
+            $entityManager->persist($site);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('place', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('site/new.html.twig', [
+            'site' => $site,
+            'formSite' => $formSite,
+        ]);
+    }
+
 }
