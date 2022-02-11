4afca5760515335ec16a6df2e850af3fdcf1c847
ikram2210 <69350259+ikram2210@users.noreply.github.com>
2022-02-11 17:33:34 +0100

    Modifier l'utilisateur back

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ProfilEditType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +19,22 @@ class ProfilController extends AbstractController
     ): Response
     {
         return $this->render('profil/index.html.twig', compact("user"));
+    }
+
+    #[Route('/profil/{id}/edit', name: 'profil_edit')]
+    public function editProfile(Request $request, EntityManagerInterface $entityManager, User $user): Response
+    {
+        $form = $this->createForm(ProfilEditType::class,$user);
+        $form->handleRequest($request);
+        if($form->isSubmitted()&& $form->isValid()){
+            $entityManager->flush();
+            $this->addFlash('message','Profil mis à jour');
+            return $this->redirectToRoute('profil', ["id"=> $user->getId()]);
+        }
+        return $this->render('profil/editProfil.html.twig', [
+            'form'=> $form->createView(),
+            'user'=>$user
+
+        ]);
     }
 }
