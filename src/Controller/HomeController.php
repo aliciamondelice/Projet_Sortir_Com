291f049185ee6cd1189d5503f76e6dd965bf39cb
aliciamondelice <mondelice.alicia@gmail.com>
2022-02-13 15:53:12 +0100

    Petites corrections home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Trip;
 use App\Entity\User;
+use App\Repository\SiteRepository;
 use App\Repository\StateRepository;
 use App\Repository\TripRepository;
 use App\Repository\UserRepository;
@@ -16,12 +17,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(TripRepository $tripRepository, StateRepository  $stateRepository, TripController $tripController, EntityManagerInterface $entityManager): Response
+    public function index(SiteRepository $siteRepository, TripRepository $tripRepository, StateRepository  $stateRepository, TripController $tripController, EntityManagerInterface $entityManager): Response
     {
+        $sites = $siteRepository->findAll();
         $trip = $tripRepository->findAll();
         $tripController->updateState($trip,$stateRepository, $entityManager);
         return $this->render('home/index.html.twig', [
             'trips' => $trip,
+            'sites' => $sites
         ]);
     }
 }
